3b8ca5b39b9f3fdc5b42352ea356b41d9f38ac53
Igal Koshevoy <igal@pragmaticraft.com>
2012-06-18 17:55:06 -0700

    Improve stow recipe to include alternative syntaxes.

--- cookbooks/antipackaging/recipes/stow.rb
+++ cookbooks/antipackaging/recipes/stow.rb
@@ -3,6 +3,8 @@ stow_package "nginx" do
   check "sbin/nginx"
   url "https://s3.amazonaws.com/igalfiles/#{@name}-#{@version}.tar.gz"
   install "./configure --prefix=@@PREFIX@@ && make && make install"
+  ### Same thing using a Ruby lambda:
+  # install lambda { |prefix| "./configure --prefix=#{prefix} && make && make install" }
   dependencies %w[build-essential libpcre3-dev]
 end
 
@@ -10,6 +12,8 @@ stow_package "ts" do
   version "0.7.3"
   check "bin/ts"
   url "https://s3.amazonaws.com/igalfiles/#{@name}-#{@version}.tar.gz"
-  install lambda { |prefix| "sed -i 's/PREFIX\?=\\/usr\\/local/PREFIX?=#{prefix.gsub(/\//, '\\/')}/' Makefile && make && make install" }
+  install lambda { |prefix| "sed -i 's/^PREFIX\?=\\/usr\\/local$/PREFIX?=#{prefix.gsub(/\//, '\\/')}/' Makefile && make && make install" }
+  ### Same thing using a fancy, newfangled sed that allows custom search delimiters:
+  # install lambda { |prefix| "sed -i 's|^PREFIX\?=\/usr\/local$|PREFIX?=#{prefix}|' Makefile && make && make install" }
   dependencies %w[build-essential]
 end
